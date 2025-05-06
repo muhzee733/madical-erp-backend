43504b365e063b678b92c1db1683e1b05b109977
Basitali326 <basatali326@gmail.com>
2025-05-06 22:33:16 +0400

    asded

--- madical/madical/settings.py
+++ madical/madical/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'NAME': 'database-2',
         'USER': 'admin',
         'PASSWORD': 'Mv8XV4cDx8uwN9OFpgfx',
-        'HOST': 'database-2.c502o6q2ecjb.eu-north-1.rds.amazonaws.com',
+        'HOST': 'database-1.c502o6q2ecjb.eu-north-1.rds.amazonaws.com',
         'PORT': '3306',
     }
 }

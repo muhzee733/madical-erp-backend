3c4172db94b65d9b355d3c280d987230066e43bf
khuang1110 <k.huang1110@gmail.com>
2025-05-15 23:35:45 +1000

    Add download_url to PrescriptionSerializer for PDF export

--- madical/prescriptions/serializers.py
+++ madical/prescriptions/serializers.py
@@ -1,19 +1,30 @@
 from rest_framework import serializers
-from .models import Drug, Prescription, PrescriptionDrug
+from .models import Prescription, PrescriptionDrug, Drug
 
 class DrugSerializer(serializers.ModelSerializer):
     class Meta:
         model = Drug
         fields = '__all__'
 
+
 class PrescriptionDrugSerializer(serializers.ModelSerializer):
+    drug = DrugSerializer(read_only=True)
+
     class Meta:
         model = PrescriptionDrug
-        fields = '__all__'
+        fields = ['drug', 'dosage', 'instructions', 'quantity', 'repeats']
+
 
 class PrescriptionSerializer(serializers.ModelSerializer):
     prescribed_drugs = PrescriptionDrugSerializer(many=True, read_only=True)
+    download_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Prescription
-        fields = '__all__'
+        fields = [
+            'id', 'doctor', 'patient', 'created_at', 'notes',
+            'prescribed_drugs', 'download_url'
+        ]
+
+    def get_download_url(self, obj):
+        return f"/prescriptions/pdf/{obj.id}/"

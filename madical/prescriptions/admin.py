19d26c7a73d8499d03b19f4ba40ba17ccf244864
khuang1110 <k.huang1110@gmail.com>
2025-05-15 23:38:34 +1000

    Add PDF export link to PrescriptionAdmin in Django Admin

--- madical/prescriptions/admin.py
+++ madical/prescriptions/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Prescription, PrescriptionDrug, Drug
 
-# Register your models here.
+@admin.register(Drug)
+class DrugAdmin(admin.ModelAdmin):
+    list_display = ['name', 'form', 'strength', 'is_schedule_8']
+    search_fields = ['name', 'form']
+
+
+@admin.register(PrescriptionDrug)
+class PrescriptionDrugAdmin(admin.ModelAdmin):
+    list_display = ['prescription', 'drug', 'dosage', 'quantity', 'repeats']
+
+
+@admin.register(Prescription)
+class PrescriptionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'doctor', 'patient', 'created_at', 'is_final', 'pdf_link']
+    readonly_fields = ['created_at']
+
+    def pdf_link(self, obj):
+        return format_html(
+            '<a class="button" href="/prescriptions/pdf/{}/" target="_blank">Download PDF</a>',
+            obj.id
+        )
+    pdf_link.short_description = "Export PDF"

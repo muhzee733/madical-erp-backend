0e30bce0976ed46a2bda8fa81cf09da805dc17c3
Ivan Wen <ivan.wen@mycommunitydirectory.com.au>
2025-05-21 23:44:39 +1000

    add IsDoctorOrAdmin permissions into api/v1/drug/ endpoint

--- madical/users/permissions.py
+++ madical/users/permissions.py
@@ -14,4 +14,10 @@ class IsPatient(BasePermission):
         if not user or not user.is_authenticated:
             return False
         return user.role == 'patient'or request.user.is_superuser
+    
+class IsDoctorOrAdmin(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and (
+            request.user.role in ['doctor', 'admin'] or request.user.is_superuser
+        )
 

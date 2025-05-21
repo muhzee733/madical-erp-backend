39af8adba1dd95834d1c68c5d9f86e9f6fccfb19
Ivan Wen <ivan.wen@mycommunitydirectory.com.au>
2025-05-21 23:33:06 +1000

    add pagination

--- madical/madical/settings.py
+++ madical/madical/settings.py
@@ -133,6 +133,8 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
 SIMPLE_JWT = {

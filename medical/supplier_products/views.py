12acc6585e8256afa69b570923f332fb9d573d26
khuang1110 <k.huang1110@gmail.com>
2025-06-01 23:53:22 +1000

    Add detail endpoints for retrieving user, drug, and supplier product by ID

--- medical/supplier_products/views.py
+++ medical/supplier_products/views.py
@@ -1,7 +1,13 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import SupplierProduct
 from .serializers import SupplierProductSerializer
+
+class SupplierProductDetailView(generics.RetrieveAPIView):
+    queryset = SupplierProduct.objects.all()
+    serializer_class = SupplierProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class SupplierProductListView(generics.ListAPIView):
     queryset = SupplierProduct.objects.all().order_by('id')
     serializer_class = SupplierProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
